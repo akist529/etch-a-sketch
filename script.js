eb7887432eb9f731f75078077d8e4e0bf9dc7908
Alex Kist <akist0707@gmail.com>
2022-03-31 20:29:26 -0500

    Create container to hold divs

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // JAVASCRIPT FOR ETCH-A-SKETCH
 
-let div = document.createElement('div');
-div.textContent = 'Div';
+let body = document.querySelector('body');
 
-for (let i = 0; i < 4; i++)
+body.id = 'body';
+body.style.display = flex;
+
+let container = document.createElement('div');
+
+document.getElementById('body').appendChild(container);
+container.id = 'container';
+
+let div = document.createElement('div');
+
+for (let i = 1; i < 5; i++)
 {
     const row = div.cloneNode(true);
-    document.getElementById('body').appendChild(row);
+    document.getElementById('container').appendChild(row);
     row.id = `row${i}`;
 
-    for (let j = 0; j < 3; j++)
+    for (let j = 1; j < 4; j++)
     {
         const column = div.cloneNode(true);
         document.getElementById(`row${i}`).appendChild(column);
+        column.id = `column${j}`;
     }
 }
