7ea5fa51e0c34b224fb7880fa77b551841a573c5
Alex Kist <akist0707@gmail.com>
2022-03-31 22:00:52 -0500

    Further changes to script.js

--- script.js
+++ script.js
@@ -3,25 +3,27 @@
 let body = document.querySelector('body');
 
 body.id = 'body';
-body.style.display = flex;
 
 let container = document.createElement('div');
+container.style.display = "flex";
 
 document.getElementById('body').appendChild(container);
 container.id = 'container';
 
 let div = document.createElement('div');
+div.textContent = "div";
+div.style.border = "5px solid black";
 
 for (let i = 1; i < 5; i++)
 {
-    const row = div.cloneNode(true);
-    document.getElementById('container').appendChild(row);
-    row.id = `row${i}`;
+    const column = div.cloneNode(true);
+    document.getElementById('container').appendChild(column);
+    column.id = `column${i}`;
 
     for (let j = 1; j < 4; j++)
-    {
-        const column = div.cloneNode(true);
-        document.getElementById(`row${i}`).appendChild(column);
-        column.id = `column${j}`;
+    { 
+        const row = div.cloneNode(true);
+        document.getElementById(`column${i}`).appendChild(row);
+        row.id = `row${j}`;
     }
 }
